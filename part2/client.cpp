f8921f8cc9226ed2579acc9ed08778e86d31f61b
Rahul Kumar <rk32298@gmail.com>
2020-02-21 14:23:36 -0500

    Update client.cpp

--- part2/client.cpp
+++ part2/client.cpp
@@ -1,5 +1,4 @@
 // Client side C/C++ program to demonstrate Socket programming
-// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -27,24 +26,43 @@ int main(int argc, char const *argv[])
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock, argv[1],strlen(argv[1]),0);
+    int val = send(sock, argv[1],strlen(argv[1]),0);
     char buf[1000];
     int numbytes = 1000;
-    while(numbytes > 0) {
-        numbytes = recv(sock, buf, 1000 - 1, 0);
-        buf[numbytes] = '\0';
-        printf("IP ADDRESS%s",buf);
-    }
+    recv(sock, &buf,1000,0);
+    printf("%s\n",buf);
+    while (true) {
     char i;
     string ipaddr;
     std::cout << "Enter Valid IP Address To Send: ";
     getline(std::cin, ipaddr,std::cin.widen('\n'));
-    string slash("\\");
-    ipaddr.append(slash);
-    send(sock, &ipaddr,ipaddr.length(),0);
     string message;
     std::cout << "Enter Message To Send: ";
     getline(std::cin, message);
+
+    int sock1, valread1;
+    struct sockaddr_in serv_addr1 = c->create_address((const char *)&ipaddr,PORT);
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if (listen(sock1, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+    int new_socket = c->accept1(sock, c->address, sizeof (c->address));
+    if ((sock1 = c->create_socket()) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+    if (c->connection(sock1, serv_addr1) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+}
+
 
     return 0;
 }
